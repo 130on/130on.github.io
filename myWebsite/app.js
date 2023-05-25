0eb0f0e06a261f03b8403e16abcc5d855effd5fb
130on <97154163+130on@users.noreply.github.com>
2023-05-24 17:53:26 -1000

    Uploaded AAS Diploma

---
+++ myWebsite/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* 
+
+Name: Alon Saar 
+Date: 5/23/2023
+Description: script that handles different events in the website
+
+
+*/
+
+
+
+
+// add a preventfefault for the form
+
+// add an interactive display of my education diplomas
+
